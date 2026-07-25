f29228af8e9177e7d87bd6936cce4e18dd1a339d
terdphun <terdphun@gmail.com>
2026-07-25 09:29:34 +0700

    Update LIFF_ID to remove extra characters

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const LIFF_ID=2010839050-O3sJnlZe;
+const LIFF_ID=2010839050;
 async function init(){try{await liff.init({liffId:LIFF_ID});if(!liff.isLoggedIn()){liff.login();return;}
 const p=await liff.getProfile();displayName.value=p.displayName;userId.value=p.userId;
 navigator.geolocation.getCurrentPosition(pos=>{latitude.value=pos.coords.latitude;longitude.value=pos.coords.longitude;});
